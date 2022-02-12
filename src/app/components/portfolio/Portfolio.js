86075088afdf3fef4c24f732cfef7ad72711e523
Marc Hosecloth <marc.hosecloth@gmail.com>
2022-02-12 00:46:01 -0500

    replace Dropbox download link with Google Docs

--- src/app/components/portfolio/Portfolio.js
+++ src/app/components/portfolio/Portfolio.js
@@ -4,7 +4,7 @@ function Portfolio() {
   return (
     <row className={"Portfolio"}>
       <td className={"Portfolio-item"}>
-        <a href={"https://docs.google.com/document/d/1bYLZiCjJV6KxfI2NRlgR1ABSefKNOT5X/edit"}
+        <a href={"https://docs.google.com/document/d/1bYLZiCjJV6KxfI2NRlgR1ABSefKNOT5X/edit"} target="_blank"
            className={"Portfolio-item-text"}>Resume</a>
       </td>
       <td className={"Portfolio-item"}>
