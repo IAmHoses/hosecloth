94b4fc96fcc7944578fd733699cb26f3e604f139
Marc Hosecloth <marc.hosecloth@gmail.com>
2022-02-17 23:50:32 -0500

    update resume href to Dropbox and remove _blank targets

--- src/app/components/portfolio/Portfolio.js
+++ src/app/components/portfolio/Portfolio.js
@@ -4,15 +4,15 @@ function Portfolio() {
   return (
     <row className={"Portfolio"}>
       <td className={"Portfolio-item"}>
-        <a href={"https://docs.google.com/document/d/1bYLZiCjJV6KxfI2NRlgR1ABSefKNOT5X/edit"} target="_blank"
+        <a href={"https://www.dropbox.com/s/89hfdn8hf0eldbz/Hosecloth%20Resume.pdf?dl=0"}
            className={"Portfolio-item-text"}>Resume</a>
       </td>
       <td className={"Portfolio-item"}>
-        <a href={"https://www.linkedin.com/in/marc-hosecloth/"} target="_blank"
+        <a href={"https://www.linkedin.com/in/marc-hosecloth/"}
            className={"Portfolio-item-text"}>LinkedIn</a>
       </td>
       <td className={"Portfolio-item"}>
-        <a href={"https://github.com/TheHosecloth"} target="_blank" className={"Portfolio-item-text"}>Github</a>
+        <a href={"https://github.com/TheHosecloth"} className={"Portfolio-item-text"}>Github</a>
       </td>
     </row>
   );
