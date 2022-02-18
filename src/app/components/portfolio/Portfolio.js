e5f706e390b95bec1319c71e18e1e23b897b7bbc
Marc Hosecloth <marc.hosecloth@gmail.com>
2022-02-18 00:01:49 -0500

    add _blank targets with proper security

--- src/app/components/portfolio/Portfolio.js
+++ src/app/components/portfolio/Portfolio.js
@@ -5,14 +5,18 @@ function Portfolio() {
     <row className={"Portfolio"}>
       <td className={"Portfolio-item"}>
         <a href={"https://www.dropbox.com/s/89hfdn8hf0eldbz/Hosecloth%20Resume.pdf?dl=0"}
+           target={"_blank"} rel={"noreferrer"}
            className={"Portfolio-item-text"}>Resume</a>
       </td>
       <td className={"Portfolio-item"}>
         <a href={"https://www.linkedin.com/in/marc-hosecloth/"}
+           target={"_blank"} rel={"noreferrer"}
            className={"Portfolio-item-text"}>LinkedIn</a>
       </td>
       <td className={"Portfolio-item"}>
-        <a href={"https://github.com/TheHosecloth"} className={"Portfolio-item-text"}>Github</a>
+        <a href={"https://github.com/TheHosecloth"}
+           target={"_blank"} rel={"noreferrer"}
+           className={"Portfolio-item-text"}>Github</a>
       </td>
     </row>
   );
