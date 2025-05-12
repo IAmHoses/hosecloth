b6224831181e548e9bae17aaeed75584d445aa19
Marc Hosecloth <hoses@macbookpro.lan>
2025-05-11 20:49:12 -0400

    2025 updates for Netlify migration

--- src/app/components/portfolio/Portfolio.js
+++ src/app/components/portfolio/Portfolio.js
@@ -4,7 +4,7 @@ function Portfolio() {
   return (
     <row className={"Portfolio"}>
       <td className={"Portfolio-item"}>
-        <a href={"https://docs.google.com/document/d/1aQ4bwmC_TGeEbw8UnSzWIaTwTKmZzCVP5rvbmi6d0a8/edit?usp=sharing"}
+        <a href={"https://docs.google.com/document/d/19Vv8zqr0573EDGojpB_L4x-bT2l-Zjo-kA_9NC5_ESQ/edit?usp=sharing"}
            target={"_blank"} rel={"noreferrer"}
            className={"Portfolio-item-text"}>Resume</a>
       </td>
@@ -14,7 +14,7 @@ function Portfolio() {
            className={"Portfolio-item-text"}>LinkedIn</a>
       </td>
       <td className={"Portfolio-item"}>
-        <a href={"https://github.com/TheHosecloth"}
+        <a href={"https://github.com/IAmHoses"}
            target={"_blank"} rel={"noreferrer"}
            className={"Portfolio-item-text"}>Github</a>
       </td>
